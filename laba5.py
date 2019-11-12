5b27f0a6e9b5649bc1329f0b6b9e754b2cbff967
alnkravchenko <alenakravchenko1@gmail.com>
2019-11-12 19:49:47 +0200

    Fixed conditions for coprime numbers

--- laba5.py
+++ laba5.py
@@ -1,12 +1,24 @@
-
 print('Кравченко О.О. ІС-93 Лабораторна рoбота №5 Варіант 13')
 
-firstNum = int(input('Enter number p: ')) # p initialization 
+firstNum = int(input('Enter number p: ')) # p initialization
 secondNum = int(input('Enter number q: ')) # q initialization
-dividers = [] # List with all needed dividers
+result = [] # List with all needed dividers
 
-for part in range(firstNum + 1): # Loop for searching dividers
-    divider = part + 1 # Because loop starts from 0
-    if firstNum % divider == 0 and secondNum % divider != 0 and divider % secondNum != 0: # Conditions for divider of first number and for checking coprime numbers (divider and second number)
-        dividers.append(str(divider)) # Add divider into list of all dividers
-print('All dividers for p and coprime numbers for q:', ', '.join(dividers))
+def dividers (number):  # Function for searching dividers
+    divArray = []
+    for divider in range(number + 1): # Loop for searching number's dividers
+        if number % (divider + 1) == 0: # Condition for number's divider
+            divArray.append(divider + 1)
+    return set(divArray)
+
+firstNumDividers = dividers(firstNum)  # All p's dividers
+firstNumDividers.remove(1)  # Because first divider is always"1"
+secondNumDividers = dividers(secondNum)  # All q's dividers
+secondNumDividers.remove(1)  # Because first divider is always "1"
+
+for element in firstNumDividers:  # Loop for searching dividers of p's divider
+    elementDividers = dividers(element)
+    if not elementDividers & secondNumDividers:  # Finding common dividers
+        result.append(str(element))  # Add divider into list of all dividers
+
+print('All dividers for p and coprime numbers for q:', ', '.join(result))
