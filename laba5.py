73d0d60f52593d71bf634e33224a7f75ff5e6e1b
alnkravchenko <alenakravchenko1@gmail.com>
2019-11-22 08:46:39 +0200

    Added alternative realization without embedded function

--- laba5.py
+++ laba5.py
@@ -9,16 +9,32 @@ def dividers (number):  # Function for searching dividers
     for divider in range(number + 1): # Loop for searching number's dividers
         if number % (divider + 1) == 0: # Condition for number's divider
             divArray.append(divider + 1)
-    return set(divArray)
+    return divArray
+    # return set(divArray) # When we use code with embedded function
 
 firstNumDividers = dividers(firstNum)  # All p's dividers
 firstNumDividers.remove(1)  # Because first divider is always"1"
 secondNumDividers = dividers(secondNum)  # All q's dividers
 secondNumDividers.remove(1)  # Because first divider is always "1"
 
-    for element in firstNumDividers:  # Loop for searching dividers of p's divider
+"""
+# Code with embedded function
+for element in firstNumDividers:  # Loop for searching dividers of p's divider
     elementDividers = dividers(element)
     if not elementDividers & secondNumDividers:  # Finding common dividers
         result.append(str(element))  # Add divider into list of all dividers
+"""
+
+flag = True
+
+for element in firstNumDividers:  # Loop for searching dividers of p's divider
+    elementDividers = dividers(element)
+    for item in elementDividers: # Compare dividers of p's divider with q's dividers
+        for number in secondNumDividers:
+            if item == number: # If they have common dividers then p's divider and q aren't coprime numbers
+                flag = False
+                break
+    if flag: 
+        result.append(str(element))
 
 print('All dividers for p and coprime numbers for q:', ', '.join(result))
