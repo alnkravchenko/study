c4381e086d7c6f1fc94d7f0cc4e6bb71885ac05f
alnkravchenko <alnkravchenko@github.com>
2019-12-16 18:21:12 +0200

    Deleted extra whitespaces

--- laba8.py
+++ laba8.py
@@ -1,20 +1,20 @@
 import random
 
-print('Кравченко О.О. ІС-93 Лабораторна рoбота №6 Варіант 13')
+print('Кравченко О.О. ІС-93 Лабораторна рoбота №8 Варіант 13')
 
-originalArray = [] # Array with random numbers
-modifiedArray = [] # Result of modifying originalArray in loops
-matrix = [] # Modified matrix from the task
+originalArray = []  # Array with random numbers
+modifiedArray = []  # Result of modifying originalArray in loops
+matrix = []  # Modified matrix from the task
 array = []
-flag = False # Flag for breaking the loop when condition from our task is true
+flag = False  # Flag for breaking the loop when condition from our task is true
 
-
-def input_size(): # Input size of matrix
+def input_size():  # Input size of matrix
     number = int(input('Enter size of matrix: '))
     return number
 
-def create_matrix(capacity): # Creating matrix from the task
-    for index in range(capacity): # Row with random real numbers with 2 digits after the dot
+def create_matrix(capacity):  # Creating matrix from the task
+    # Row with random real numbers with 2 digits after the dot
+    for index in range(capacity):
         originalArray.append(round(random.uniform(-100.0, 100.0), 2))
 
     print('Original array:', originalArray)
@@ -22,20 +22,20 @@ def create_matrix(capacity): # Creating matrix from the task
     modifiedArray = originalArray.copy()
     matrix.append(originalArray)
     for row in range(capacity):
-        if modifiedArray[0] != originalArray[-1]: # Condition from our task
-            modifiedArray.append(modifiedArray.pop(0))  #
+        if modifiedArray[0] != originalArray[-1]:  # Condition from our task
+            modifiedArray.append(modifiedArray.pop(0))
             array = modifiedArray.copy()
             matrix.append(array)
             print(f'{row+1} changing: {array}')
-        # print(matrix[index][element], end= ' ')
 
-def output_matrix(): # Output matrix
+def output_matrix():  # Output matrix
     print('Our matrix:')
     for row in matrix:
         print('|', end=' ')
         for number in row:
-            print('%6.2f' %number, end =' ')
+            print('%6.2f' % number, end=' ')
         print('|')
+
 
 size = input_size()
 create_matrix(size)
